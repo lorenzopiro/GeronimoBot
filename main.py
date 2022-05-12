2e748316417388416b7b16695bae6ac681ae2063
pirol <prilnz99p12d086f@studenti.unical.it>
2022-05-13 00:17:13 +0200

    Aggiunta prima parte comando aggiungiSito

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-
 API_KEY = creds.API_KEY
 
 bot = telebot.TeleBot(API_KEY)   
@@ -37,24 +36,28 @@ def get_soup(url):
     
     else:
         print(response.status_code)
-
-@bot.message_handler(commands=['greet'])
-def greet(message):
-    bot.reply_to(message, "ciao")
-
-@bot.message_handler(commands=['hey'])
-def greet(message):
-    bot.send_message(message.chat.id, "hey")
-    
+        
 def urlCheck(message):
     pattern1 = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.\w+\/?"
     pattern2 = r"[-a-zA-Z0-9]{1,256}\.[a-zA-Z0-9()]{1,6}"
-    if re.match(pattern1, str(message.text)) or re.match(pattern2, str(message.text)):
+    if re.match(pattern1, message.text) or re.match(pattern2, message.text):
         bot.send_message(message.chat.id, "link")
         return True
 
     return False
 
+
+
+
+
+@bot.message_handler(commands=['start'])#Registra l'utente nel database
+def start(message):
+    user = message.chat.id
+    db.collection('Utente').add({'nome': user})
+    bot.send_message(message.chat.id, "Benvenuto, io sono Geronimo utilizza i comandi per poter tracciare siti web e prodotti: ")
+
+
+    
 @bot.message_handler(func=urlCheck) #CONTROLLA CHE SIA UN URL E CHE SIA VALIDO
 def mioSend(message):
     URL = message.text
@@ -93,27 +96,43 @@ def list(message):
     pass
 
 #COMANDO /addURL
-@bot.message_handler(commands=['aggiungiURL'])
+@bot.message_handler(commands=['aggiungisito'])
 def add(message):
-    pass
+    msg = bot.send_message(message.chat.id, "Bene, mandami qui di seguito l'URL del sito che vuoi monitorare: ")
+    bot.register_next_step_handler(msg, addStep2)
+
+def addStep2(message):
+    if urlCheck(message):
+        try:
+            urlSito = message.text
+            html = get_soup(urlSito)
+            db.collection('Sito').add({'url': urlSito, 'html': html})
+            db.collection('Utente-Sito').add({'sito':urlSito, 'utente':message.chat.id})
+            bot.send_message(message.chat.id, "Url aggiunto con successo")
+
+        except Exception as e:
+            bot.reply_to(message, str(e))
+        
+        
+    
 
 #COMANDO /removeURL
-@bot.message_handler(commands=['rimuoviURL'])
+@bot.message_handler(commands=['rimuovisito'])
 def remove(message):
     pass
 
 #COMANDO /addProduct
-@bot.message_handler(commands=['aggiungiProdotto'])
+@bot.message_handler(commands=['aggiungiprodotto'])
 def addProduct(message):
     pass
 
 #COMANDO /removeProduct
-@bot.message_handler(commands=['rimuoviProdotto'])
+@bot.message_handler(commands=['rimuoviprodotto'])
 def removeProduct(message):
     pass
 
 #COMANDO /productsList
-@bot.message_handler(commands=['listaProdotti'])
+@bot.message_handler(commands=['listaprodotti'])
 def productsList(message):
     pass
 
@@ -125,16 +144,14 @@ itembtn3 = types.KeyboardButton('c')
 itembtn4 = types.KeyboardButton('d')
 markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Scegli un'opzione:", reply_markup=markup)
 
 
 
 
 
 
-bot.polling()   
+
+bot.infinity_polling()   
 
 
 
